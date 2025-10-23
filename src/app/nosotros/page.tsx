285703a62c34631f16325d224ccad713cbe77f8f
Henrry Tandaypan <htandaypan@attachconsultores.com>
2025-10-23 18:46:54 -0500

    Agregar page nosotros

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
+
+import styles from "./nosotros.module.css";
 
 export default function Home() {
   // Estado para manejar el caso activo
@@ -68,7 +71,7 @@ export default function Home() {
         </header>
 
         {/* Hero Section */}
-        <div className="z-10 pb-20">
+        <div className={`z-10 pb-20 ${styles.hero}`}>
           <div className="ml-15 border-l-2 border-b-2 border-[#1e3fda]/20 rounded-bl-[50px] pt-20">
             <div className="grid lg:grid-cols-2 gap-12 items-center ml-15">
               {/* Content */}
@@ -82,9 +85,7 @@ export default function Home() {
 
               {/* Image placeholder - where the person and city image goes */}
               <div className="">
-                <div className="bg-black/10 backdrop-blur-sm rounded-2xl p-8 h-96 flex items-center justify-center">
-                  <span className="text-white/60 text-lg">Banner Image Placeholder</span>
-                </div>
+                  <Image src="/attach-group/images/nosotros/foto-grupal-attach.png" className="w-full" alt="icon Galileo IA" width={684} height={580} />
               </div>
             </div>
           </div>
@@ -117,7 +118,7 @@ export default function Home() {
         </section>
 
         {/* Services Section */}
-        <div className="py-20">
+        <div className={`py-20 ${styles.servicios}`}>
           <div className="mx-30">
             {/* Header */}
             <div className="mb-10 text-center ">
@@ -132,12 +133,13 @@ export default function Home() {
               <div className="bg-white rounded-3xl shadow-lg overflow-hidden">
                 {/* Logo Section */}
                 <div className="bg-linear-to-br rounded-3xl from-red-400 to-orange-400 h-60 flex items-center justify-center p-8">
+                  <Image src="/attach-group/images/nosotros/logo-galilea.png" alt="icon Galileo IA" width={279} height={74} />
                 </div>
 
                 {/* Content Section */}
                 <div className="p-8 text-gray-600">
                   <p className="text-[1.3rem] mb-6">
-                    Especialistas en inteligencia artificial, cloud, martech y analítica avanzada para acelerar el crecimiento de las organizaciones
+                    Especialistas en inteligencia artificial, cloud, martech y analítica avanzada para acelerar el crecimiento de las organizaciones.
                   </p>
                 </div>
               </div>
@@ -146,12 +148,13 @@ export default function Home() {
               <div className="bg-white rounded-3xl shadow-lg overflow-hidden">
                 {/* Logo Section */}
                 <div className="bg-linear-to-br rounded-3xl from-lime-400 to-green-400 h-60 flex items-center justify-center p-8">
+                  <Image src="/attach-group/images/nosotros/logo-athenaAds.png" alt="icon AthenaAds" width={300} height={83} />
                 </div>
 
                 {/* Content Section */}
                 <div className="p-8 text-gray-600">
                   <p className="text-[1.3rem] mb-6">
-                    Especialistas en inteligencia artificial, cloud, martech y analítica avanzada para acelerar el crecimiento de las organizaciones
+                    Especialistas en publicidad programática, que desarrolla soluciones personalizadas y efectivas para cada marca.
                   </p>
                 </div>
               </div>
@@ -160,12 +163,13 @@ export default function Home() {
               <div className="bg-white rounded-3xl shadow-lg overflow-hidden">
                 {/* Logo Section */}
                 <div className="bg-linear-to-br rounded-3xl from-blue-400 to-cyan-400 h-60 flex items-center justify-center p-8">
+                  <Image src="/attach-group/images/nosotros/logo-attach-media.png" alt="icon Attach Media" width={239} height={96} />
                 </div>
 
                 {/* Content Section */}
                 <div className="p-8 text-gray-600">
                   <p className="text-[1.3rem] mb-6">
-                    Especialistas en inteligencia artificial, cloud, martech y analítica avanzada para acelerar el crecimiento de las organizaciones
+                    Agencia integral de medios y soluciones digitales que combina estrategia, data y tecnología para diseñar campañas efectivas.
                   </p>
                 </div>
               </div>
@@ -176,17 +180,17 @@ export default function Home() {
 
       <div className="atm-content bg-gradient-to-b from-[#FFFFFF] to-[#F7F7F7] rounded-b-[3rem]">
         {/*Lideres Section*/}
-        <div className="py-20">
-          <h2 className="mx-30 text-center text-4xl lg:text-5xl bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent font-bold mb-6">
+        <div className={`py-20 mx-30 ${styles.lideres}`}>
+          <h2 className="text-center text-4xl lg:text-5xl bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent font-bold mb-6">
             Quiénes lideran el camino
           </h2>
           <section className="py-16">
-            <div className="mx-30">
+            <div className="">
               <div className="flex flex-wrap justify-center gap-10">
                 {/* Carlo R.*/}
                 <div className="relative">
-                  <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-                  <div className="absolute -bottom-6 left-4 bg-white rounded-2xl shadow-md px-5 py-3">
+                  <Image src="/attach-group/images/nosotros/CarloRodriguez.jpg" alt="Carlo Rodriguez" width={361} height={423} />
+                  <div className={`absolute -bottom-6 left-4 bg-white rounded-2xl shadow-md px-5 py-5 ${styles["card-lideres__content"]}`}>
                     <p className="text-lg font-semibold text-gray-500">Carlo Rodriguez</p>
                     <p className="text-sm bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent">CEO Attach</p>
                   </div>
@@ -195,8 +199,8 @@ export default function Home() {
 
                 {/*Fernando S.*/}
                 <div className="relative">
-                  <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-                  <div className="absolute -bottom-6 left-4 bg-white rounded-2xl shadow-md px-5 py-3">
+                  <Image src="/attach-group/images/nosotros/fernando-salazar.jpg" alt="Fernando Salazar" width={361} height={423} />
+                   <div className={`absolute -bottom-6 left-4 bg-white rounded-2xl shadow-md px-5 py-5 ${styles["card-lideres__content"]}`}>
                     <p className="text-lg font-semibold text-gray-500">Fernando Salazar</p>
                     <p className="text-sm bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent">COO Attach</p>
                   </div>
@@ -208,57 +212,21 @@ export default function Home() {
         </div>
 
         {/*Clientes Section*/}
-        <div className="py-20">
-          <h2 className="mx-30 text-center text-4xl lg:text-5xl bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent font-bold mb-6">
+        <div className={`py-20 mx-30 ${styles.clientes}`}>
+          <h2 className="text-center text-4xl lg:text-5xl bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent font-bold mb-6">
             Algunos de nuestros clientes
           </h2>
 
           <section className="py-16">
-            <div className="mx-30">
-              <div className="flex flex-wrap justify-center gap-10">
-                {/* Carlo R.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/*Fernando S.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/* Carlo R.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/*Fernando S.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/* Carlo R.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/*Fernando S.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/* Carlo R.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/*Fernando S.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/* Carlo R.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/*Fernando S.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/* Carlo R.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-
-                {/*Fernando S.*/}
-                <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
-              </div>
+            <div className="">
+              <Image className="w-full" src="/attach-group/images/nosotros/clientes-desktop.png" alt="clientes" width={1296} height={747} />
             </div>
           </section>
 
         </div>
 
         {/*Oficinas Section*/}
-        <div className="flex gap-10 mx-30 pb-20 justify-items-stretch">
+        <div className={`flex gap-10 mx-30 pb-20 justify-items-stretch ${styles["section-oficinas"]}`}>
           {/* Carlo R.*/}
           <div className="w-1/2">
           <h2 className="text-4xl lg:text-5xl bg-linear-to-r from-[#1e3fda] to-[#58308c] bg-clip-text text-transparent font-bold mb-6">
@@ -267,21 +235,23 @@ export default function Home() {
           <p className="tex-xl text-gray-400 border-1 px-3 py-2 w-45 rounded-[50px]">Perú - México - USA</p>
           </div>
           <div className="relative">
-            <span className="block w-[15rem] h-[9rem] bg-gray-200 rounded-md"></span>
+            <Image src="/attach-group/images/nosotros/mapa-atatch.png" alt="Mapa Attach" width={486} height={550} />
           </div>
 
         </div>
       </div>
       {/* CTA Talk Us */}
-      <section className="pb-17 pr-30">
+      <section className={`pb-17 pr-30 ${styles["cta-talkus"]}`}>
         <div className="flex flex-col gap-10 pl-30 pt-20 pb-17 border-r-2 border-b-2 border-white/30 rounded-br-[50px]">
           <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold max-w-5xl">
             Hablemos sobre cómo potenciar juntos el crecimiento de tu empresa
           </h2>
-
-          <button className="self-start bg-white text-[#1e3fda] font-semibold py-3 px-6 rounded-md hover:bg-opacity-90 transition">
-            Conversemos <span className="ml-2">➜</span>
-          </button>
+          <Link
+                href="/contacto"
+                className="self-start bg-white text-[#1e3fda] font-semibold py-3 px-6 rounded-md hover:bg-opacity-90 transition"
+              >
+                 Conversemos <span className="ml-2">➜</span>
+          </Link>
         </div>
       </section>
 
